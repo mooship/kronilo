e991cf8a60ac5d06e3a13b19b96343d8d48d138f
Timothy Brits <timothybrits@icloud.com>
2025-07-14 09:39:24 +0200

    Revert "Simplify manualChunks logic in Vite config"
    This reverts commit c6fc8869971ff590ba3df1230a45c933c0226e97.

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,30 @@ export default defineConfig(({ mode }) => ({
 		rollupOptions: {
 			output: {
 				manualChunks(id) {
+					if (id.includes("react-use") || id.includes("react-tooltip")) {
+						return "react-utils";
+					}
+
+					if (id.includes("react-router")) {
+						return "react-router";
+					}
+
+					if (id.includes("node_modules/react/") || id.includes("react-dom")) {
+						return "react-core";
+					}
+
+					if (id.includes("cron-parser")) {
+						return "cron-parser";
+					}
+
+					if (id.includes("cronstrue")) {
+						return "cronstrue";
+					}
+
+					if (id.includes("zustand") || id.includes("ky")) {
+						return "state-http";
+					}
+
 					if (id.includes("node_modules")) {
 						return "vendor";
 					}
